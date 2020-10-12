ab4e30574deacdafd0133ad430f53472e1cbf487
Rex <rex@pop-os.localdomain>
2020-10-12 15:38:42 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from '../src/components/Navbar/index'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Hey whats up g</h1>
     </div>
   );
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+       <>
+        <Nav>
+            <NavbarContainer>
+                <Navlogo></Navlogo>
+            </NavbarContainer>    
+        </Nav>
+       </>
+    )
+}
+
+export default Navbar
