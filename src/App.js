0d18b3b1a78aba17c251e60803f560cf06c39f9b
RexHolmstrom <rexholmstrom96@gmail.com>
2020-10-16 17:14:59 +0200

    new sidebar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Navbar from '../src/components/Navbar/index.js'
 import {BrowserRouter as Router} from 'react-router-dom'
+import Sidebar from './components/Sidebar';
+
 
 function App() {
   return (
     <Router>
+      <Sidebar />
       <Navbar />
-     </Router>
+    </Router>
   );
 }
 
