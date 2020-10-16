1b506dc66486afbc11b18914d21aff5977233e10
RexHolmstrom <rexholmstrom96@gmail.com>
2020-10-16 16:53:40 +0200

    new sidebar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <SidebarContainer>
+            <Icon>
+                <CloseIcon />
+            </Icon>
+        </SidebarContainer>
+    )
+};
+export default Sidebar;
