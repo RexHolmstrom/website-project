186ca4f1dda695643e72a4de266159537621472e
RexHolmstrom <rexholmstrom96@gmail.com>
2020-10-30 23:35:26 +0100

    route update

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { Link as LinkS } from "react-scroll";
+import { Link as LinkR } from "react-router-dom";
 import { FaTimes } from "react-icons/fa";
 
 export const SidebarContainer = styled.aside`
@@ -57,4 +59,22 @@ export const SideBtnWrap = styled.div`
   justify-content: center;
 `;
 
-export
+export const SidebarRoute = styled(LinkR)`
+  border-radius: 50px;
+  background: #01bf71;
+  white-space: nowrap;
+  padding: 16px 64px;
+  color: #010606;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+
+  &:hover {
+    transistion: all 0.2s ease-in-out;
+    background: #fff;
+    color: #010606;
+  }
+`;
