9a2699160fb1542f0bb0cd1629c38a5768ed2389
Rex <rexholmstrom96@gmail.com>
2020-11-13 14:53:51 +0000

    style update

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -29,3 +29,27 @@ export const VideoBg = styled.video`
   object-fit: cover;
   background: #232a34;
 `;
+
+export const HeroContent = styled.div`
+  z-index: 3;
+  max-width: 1200px;
+  position: absolute;
+  padding: 8px 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const HeroH1 = styled.h1`
+  color: #fff;
+  font-size: 48px;
+  text-align: center;
+
+  @media screen and (max-width: 768px) {
+    font-size: 40px;
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 32px;
+  }
+`;
