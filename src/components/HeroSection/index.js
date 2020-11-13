52c8822862f73c5ed26c94be603d6e541b69bf6d
Rex <rexholmstrom96@gmail.com>
2020-11-13 15:00:58 +0000

    fix

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Video from "../../videos/video.mp4";
-import { HeroContainer, HeroBg, VideoBg } from "./HeroElements";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
   return (
     <HeroContainer>
       <HeroBg>
@@ -15,7 +31,7 @@ const HeroSection = () => {
           your next payment.
         </HeroP>
         <HeroBtnWrapper>
-          <Button to="signup">
+          <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
             Get started {hover ? <ArrowForward /> : <ArrowRight />}
           </Button>
         </HeroBtnWrapper>
