0a4668c7898b75bf40b5c1eb57efa999074de792
Rex <rexholmstrom96@gmail.com>
2020-11-13 14:31:42 +0000

    update hero

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -10,3 +10,14 @@ export const HeroContainer = styled.div`
   position: relative;
   z-index: 1;
 `;
+
+export const HeroBg = styled.div`
+  position: absolute; 
+  top: 0; 
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
